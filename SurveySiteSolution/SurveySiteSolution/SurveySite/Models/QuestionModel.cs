13760cf5ec1a74480707c1447b9994870a7062ad
Luna <Hanken95@gmail.com>
2021-02-03 12:18:13 +0100

    Basic layout

---
+++ SurveySiteSolution/SurveySiteSolution/SurveySite/Models/QuestionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveySite.Models
+{
+    public enum QuestionType { Text, Radio, MultipleChoice }
+    public class QuestionModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public QuestionType QuestionType { get; set; }
+        public string Question { get; set; }
+        public List<string> Answers { get; set; }
+
+    }
+}
